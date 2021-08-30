7563681a706758c8abba0b8e66603edbf3f9f769
Evgenii <Skippik1111@gmail.com>
2021-08-30 16:03:07 +0300

    feat: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        while(min <= max) {
+            this.arr[min] = min;
+            min++;
+        }
+        this.max = this.arr.length - 1;
     }
 
     guess() {
-
+        this.middle = Math.ceil ((this.min + this.max) / 2);
+        return this.arr[this.middle];
     }
 
     lower() {
-
+        return this.max = this.middle;
     }
 
     greater() {
-
+        return this.min = this.middle;
     }
 }
 
